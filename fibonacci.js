f0efb372b84c8bb5c90d894bcd1e5e5636f53ec2
muktarkabir <mukhtarkabir3394@gmail.com>
2025-08-19 14:47:49 +0100

    Add recursive fibonacci method, not happy.

--- fibonacci.js
+++ fibonacci.js
@@ -3,7 +3,6 @@ const fibs = (num) => {
   let current = 1;
   let previous = 0;
   sequence.push(previous, current);
-
   while (num > 2) {
     sequence.push(current + previous);
     current = sequence.at(-1);
@@ -14,4 +13,18 @@ const fibs = (num) => {
   return sequence;
 };
 
-console.log(fibs(8));
+console.log(fibs(10));
+
+const fibsRecursive = (num) => {
+    if (num == 0) {
+        return [0];
+    }
+    if (num == 1) {
+        return [0,1];
+    }
+    const arr = fibsRecursive(num -1);
+    arr.push(arr.at(-1) + arr.at(-2));
+    return arr;
+};
+
+console.log(fibsRecursive(10));

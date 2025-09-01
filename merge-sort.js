dfd444b835b0b0993d13a47afa18633083c41041
muktarkabir <mukhtarkabir3394@gmail.com>
2025-09-01 12:18:08 +0100

    Refactor merge algorithm.

--- merge-sort.js
+++ merge-sort.js
@@ -1,25 +1,27 @@
 const mergeSort = (array) => {};
 
-function merge() {
-  let a = [2, 3, 6, 8, 9];
-  let b = [0, 4,4, 5, 7, 9];
-  let c = [];
+function merge(arr1, arr2) {
+  let mergedArray = [];
   let i = 0;
   let j = 0;
-  let noOfLoops = a.length + b.length;
+  let noOfLoops = arr1.length + arr2.length;
   for (let index = 0; index < noOfLoops; index++) {
-    if (a[i] < b[j]) {
-      c.push(a[i]);
+    if (arr1[i] < arr2[j]) {
+      mergedArray.push(arr1[i]);
       i++;
-    } else if (a[i] == b[j]) {
-       c.push(a[i]);
-      i++; 
+    } else if (arr1[i] == arr2[j]) {
+      mergedArray.push(arr1[i]);
+      i++;
     } else {
-      c.push(b[j]);
+      mergedArray.push(arr2[j]);
       j++;
     }
   }
 
-  console.log(c);
+  return mergedArray;
 }
-merge();
+let a = [2, 3, 6, 8, 9];
+let b = [0, 4, 4, 5, 7, 9, 11, 34];
+let c = merge(a,b);
+console.log(c);
+

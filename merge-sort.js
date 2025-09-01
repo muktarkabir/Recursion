00fc8229f52b20b5f255e14cc58790bf0f8d6f05
muktarkabir <mukhtarkabir3394@gmail.com>
2025-09-01 12:54:57 +0100

    Implement 2-way merge method.
    I've found a way to pair elements in the unsorted array as sub arrays in
    a parent array. Yes that makes sense

--- merge-sort.js
+++ merge-sort.js
@@ -22,6 +22,24 @@ function merge(arr1, arr2) {
 }
 let a = [2, 3, 6, 8, 9];
 let b = [0, 4, 4, 5, 7, 9, 11, 34];
-let c = merge(a,b);
-console.log(c);
+let c = merge(a, b);
+// console.log(c);
 
+function twoWayMergeSort(unsortedArray) {
+  let bigArray = [];
+  let elem1 = 0;
+  let elem2 = 1;
+  //rounding up accounts for uneven number of elements
+  let noOfPairs = Math.round(unsortedArray.length / 2);
+  for (let i = 0; i < noOfPairs; i++) bigArray.push([]);
+  for (let index = 0; index < bigArray.length; index++) {
+    bigArray[index].push(unsortedArray[elem1]);
+    if (unsortedArray[elem2]) bigArray[index].push(unsortedArray[elem2]);
+    elem1 += 2;
+    elem2 += 2;
+  }
+
+  console.log(bigArray);
+}
+
+twoWayMergeSort([9, 3, 5, 7, 4]);

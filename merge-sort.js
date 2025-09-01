defda4c84eb602d173feb50b3cbb8cc3d91b05cf
muktarkabir <mukhtarkabir3394@gmail.com>
2025-09-02 00:29:48 +0100

    Implement 2nd Pass for the two way sort algorithm.

--- merge-sort.js
+++ merge-sort.js
@@ -7,7 +7,6 @@ function merge(arr1, arr2) {
   let k = 0;
   let m = arr1.length - 1;
   let n = arr2.length - 1;
-  let noOfLoops = m + n;
 
   while (i <= m && j <= n) {
     if (arr1[i] < arr2[j]) {
@@ -22,19 +21,6 @@ function merge(arr1, arr2) {
   }
   for (; i <= m; i++) mergedArray[k++] = arr1[i];
   for (; j <= n; j++) mergedArray[k++] = arr2[j];
-  // for (let index = 0; index < noOfLoops; index++) {
-  //   if (arr1[i] < arr2[j]) {
-  //     mergedArray.push(arr1[i]);
-  //     i++;
-  //   } else if (arr1[i] == arr2[j]) {
-  //     mergedArray.push(arr1[i]);
-  //     i++;
-  //   } else {
-  //     mergedArray.push(arr2[j]);
-  //     j++;
-  //   }
-  // }
-
   return mergedArray;
 }
 let a = [2, 3, 6, 9, 13, 98];
@@ -42,10 +28,12 @@ let b = [0, 4, 4, 5, 7, 11, 13, 97];
 let c = merge(a, b);
 // console.log(c);
 let d = merge([8], [0, 234]);
-console.log(d);
+// console.log(d);
 
 function twoWayMergeSort(unsortedArray) {
   let bigArray = [];
+  let secondPass = [];
+  let thirdPass = [];
   let elem1 = 0;
   let elem2 = 1;
   //rounding up accounts for uneven number of elements
@@ -55,8 +43,6 @@ function twoWayMergeSort(unsortedArray) {
     //The number zero is falsy so this check prevents that
     if (typeof unsortedArray[elem2] == "number") {
       let sortedPair = merge([unsortedArray[elem1]], [unsortedArray[elem2]]);
-      // console.log(sortedPair);
-
       bigArray[index] = sortedPair;
     } else {
       bigArray[index].push(unsortedArray[elem1]);
@@ -64,10 +50,27 @@ function twoWayMergeSort(unsortedArray) {
     elem1 += 2;
     elem2 += 2;
   }
+  //reset elem variables for reuse
+  elem1 = 0;
+  elem2 = 1;
+  
+  let noOfPairsForSecondPass = Math.round(bigArray.length / 2);
+  for (let i = 0; i < noOfPairsForSecondPass; i++) secondPass.push([]);
+ for (let index = 0; index < secondPass.length; index++) {  
+    if (bigArray[elem2]) {
+      let sortedPair = merge(bigArray[elem1], bigArray[elem2]);
+      secondPass[index] = sortedPair;
+    } else {
+      secondPass[index] = bigArray[elem1];
+    }
+    elem1 += 2;
+    elem2 += 2;
+  }
+console.log(secondPass);
+  
 
-  console.log(bigArray);
+
   return bigArray;
 }
 
-let fgf = twoWayMergeSort([9, 3, 5, 7, 4, 2, 456, 0,-4]);
-console.log(fgf.at(-1).length);
+twoWayMergeSort([9, 3, 5, 7, 4, 2, 456, 0, 4]);
